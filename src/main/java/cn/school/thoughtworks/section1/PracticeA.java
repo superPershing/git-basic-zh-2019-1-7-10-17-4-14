ec8b66f7d39285b1099d1c847847d4071d80ba5b
superPershing <727342027@qq.com>
2019-03-30 02:12:11 +0800

    feat(section1PracticeA): finish section1 PracticeA

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> sameElements = new ArrayList<>();
+        for (String s : collection1) {
+            if (collection2.contains(s)) {
+                sameElements.add(s);
+            }
+        }
+        return sameElements;
     }
 }

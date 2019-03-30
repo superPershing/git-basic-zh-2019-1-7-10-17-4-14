37aefc87d9382065880845b9c5e6061a29721f72
superPershing <727342027@qq.com>
2019-03-30 21:08:29 +0800

    feat(section3PracticeC): finish section3 PracticeC

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,35 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> collection3 = conutStrings(collectionA);
+        return getNewCollection(collection3, object);
+    }
 
-        return null;
+    Map<String, Integer> conutStrings(List<String> collection1) {
+        Map<String, Integer> result = new HashMap<>();
+        for (String s : collection1) {
+            if (result.containsKey(s)) {
+                result.put(s, result.get(s) + 1);
+            } else {
+                result.put(s, 1);
+            }
+        }
+        return result;
+    }
+
+    Map<String, Integer> getNewCollection(Map<String, Integer> collection1, Map<String, List<String>> object) {
+        for (String s : object.get("value")) {
+            if (collection1.containsKey(s)) {
+                collection1.put(s, collection1.get(s) - collection1.get(s) / 3);
+            }
+        }
+        return collection1;
     }
 }
